fd0c7876f9031657c4f416c9487c6b30bb4d7572
fireduck64 <fireduck@gmail.com>
2019-08-11 22:52:55 -0700

    Working on protocol details

--- src/ChannelGlobals.java
+++ src/ChannelGlobals.java
@@ -13,19 +13,21 @@ public class ChannelGlobals
 
   public static final String NODE_TAG = "node";
 
-
-  //public static final int LONG_RANGE_POINTS=2;
-  //public static final int SHORT_RANGE_POINTS=2;
+  
+  // Always connect to this many near points
   public static final int NEAR_POINTS=6;
+
+  // Number of divisions around the ring to connect to, halving each time
   public static final int RING_DIVISONS=8;
 
 
   public static final int DHT_ELEMENT_SIZE = snowblossom.lib.Globals.BLOCKCHAIN_HASH_LEN;
 
+  // Max age to be stored.  Older data may be served but new data with signatures older than this won't be
+  // saved.
   public static final long MAX_DHT_DATA_AGE=120000L;
 
   public static final long MAX_DATA_PEER_AGE=7200L * 1000L;
-
 
 
 }
